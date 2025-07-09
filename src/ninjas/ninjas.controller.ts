e93142a2ebe55d67e111de3ea4cfba34fc59fa08
Roger <kongnyuroger711@gmail.com>
2025-07-09 19:37:37 +0100

    created controller services

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,5 +1,41 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Get, Param, Patch, Post, Query, Delete, Put } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { NinjasService } from './ninjas.service';
 @Controller('ninjas')
 export class NinjasController {
+    constructor(private readonly ninjasService: NinjasService){}
+
+    //GET/ninjas?weapon=fast --> []
+    @Get()
+    getNinjas(@Query('weapon') weapon: "stars" | "nunchucks"){
+        
+        return this.ninjasService.getNinjas(weapon)
+    
+    }
+
+    //GET/ninjas/:id
+    @Get(':id')
+     getNinja( @Param('id') id:string ){
+        return this.ninjasService.getNinja(+id);   
+    }
+    
+    //post/ninjas
+    @Post()
+    createNinja(@Body() createNinjaDto: CreateNinjaDto){
+        return this.ninjasService.createNinja(createNinjaDto)
+    };
+
+    //put/ninjas/:id
+    @Put(':id')
+     updatetNinja( @Param('id') id:string, @Body() updateNinjaDto: UpdateNinjaDto){
+        return this.ninjasService.updateNinja(+id, updateNinjaDto)
+    }
+
+    @Delete(':id')
+    removeNinja(@Param('id') id:string ){
+        return this.ninjasService.removeNinja(+id)
+    }
+
+    
 }
